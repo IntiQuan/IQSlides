6086c4d857b2aa0ecca57d0938fc072795c64edc
Henning <henning.schmidt@intiquan.com>
2020-02-20 22:31:04 +0100

    fixed bug in tests

--- tests/testthat/test_splitNumNames.R
+++ tests/testthat/test_splitNumNames.R
@@ -1,10 +1,10 @@
 library(testthat)
 library(IQSlides)
 
-test_that("splitNumNames", {
+test_that("IQSlides:::splitNumNames", {
 
   files <- c("1_bla", "2_bla_bli", "3_bla_bli_blub")
-  result <- splitNumNames(files)
+  result <- IQSlides:::splitNumNames(files)
 
   expect_equal(result$prefix, c(1, 2, 3))
   expect_equal(as.character(result$name), c("bla", "bla_bli", "bla_bli_blub"))
